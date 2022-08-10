177f68a14fb42ad6f70603650c675ba0cd4a557f
Daniel Steinke <djsteinke@gmail.com>
2022-08-09 21:08:55 -0600

    firebase

--- firebase/firebaseMessage.py
+++ firebase/firebaseMessage.py
@@ -83,5 +83,5 @@ def start_listener():
     try:
         db_trigger.listen(listener)
     except FirebaseError:
-        module_logger('failed to start listener... trying again.')
+        module_logger.error('failed to start listener... trying again.')
         start_listener()
