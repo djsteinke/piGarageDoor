1ba05dd71f59d735998e1189b8490a1621debdb3
djsteinke <djsteinke@gmail.com>
2022-10-25 10:17:17 -0600

    fix tof

--- tof.py
+++ tof.py
@@ -55,6 +55,8 @@ class TOF(object):
             module_logger.debug("start()")
             self._running = True
             self._sensor = adafruit_vl53l0x.VL53L0X(i2c, 0x29)
+            module_logger.debug("_sensor")
+            module_logger.debug(self._sensor)
             if self._start_callback is not None:
                 self._start_callback()
             threading.Timer(0.1, self.get_range).start()
